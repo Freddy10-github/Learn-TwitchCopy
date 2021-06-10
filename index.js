339df68a6b08308c0ebf94047a4c21a80e50b9f2
Freddy10-github <85281073+Freddy10-github@users.noreply.github.com>
2021-06-10 18:29:03 +0800

    placeholder

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 const getData = (cb) => {
-const Accept = 'application/vnd.twitchtv.v5+json';
-const clientId = 'luz7ktst4nb0p2ur5eee2apvgmf837';
-const limit = 20;
-const apiUrl = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=' + limit;
-//https://github.com/Lidemy/forum/discussions/138#discussion-3400596
+    const Accept = 'application/vnd.twitchtv.v5+json';
+    const clientId = 'luz7ktst4nb0p2ur5eee2apvgmf837';
+    const limit = 20;
+    const apiUrl = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=' + limit;
+    //https://github.com/Lidemy/forum/discussions/138#discussion-3400596
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", apiUrl, true);
-xhr.setRequestHeader('Accept',Accept);
-xhr.setRequestHeader('Client-id',clientId);
-xhr.send();
-xhr.onreadystatechange = function() {
-    if(this.readyState === 4 && this.status === 200){
-        var responseData =JSON.parse(this.responseText);
-        cb(null, responseData);   
-    }
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", apiUrl, true);
+    xhr.setRequestHeader('Accept',Accept);
+    xhr.setRequestHeader('Client-id',clientId);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200){
+            var responseData =JSON.parse(this.responseText);
+            cb(null, responseData);   
+        }
 }
 }
 
@@ -29,9 +29,9 @@ getData((err, data) => {
 const getColumn = data => {
     return `
     <div class="col">
-                <div class="preview"><img src="${data.preview.medium}" alt=""></div>
+                <div class="preview"><img src="${data.preview.medium}" onload="this.style.opacity=1"></div>
                 <div class="buttom">
-                    <div class="manPhoto"><img src="${data.channel.logo}" alt=""></div>
+                    <div class="manPhoto"><img src="${data.channel.logo}" onload="this.style.opacity=1"></div>
                     <div class="channel_info">
                         <div class="channel_name"> ${data.channel.status}</div>
                         <div class="channel_owner">${data.channel.display_name}</div>
